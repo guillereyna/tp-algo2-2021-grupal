e5da8e626f89670c1b4ed4ed6206f298520cb3e1
Luciana <fernandezspandau@gmail.com>
2021-06-10 17:44:36 -0300

    Esqueleto class Fichin

--- tpg4/src/Fichin.h
+++ tpg4/src/Fichin.h
@@ -1,10 +1,56 @@
-#ifndef TP4_PACALGO2_FICHIN_H
-#define TP4_PACALGO2_FICHIN_H
+#ifndef FICHIN_H
+#define FICHIN_H
 
+#include "Mapa.h"
+#include "Partida.h"
+#include "string_map.h"
+#include <vector>
+
+typedef string_map<unsigned int> ranking;
+typedef string jugador;
+typedef string direccion;
 
 class Fichin {
+public:
+    // Constructor.
+    Fichin(const Mapa&);
 
+    // Destructor.
+    ~Fichin();
+
+    // Inicia una nueva partida para el jugador.
+    void nuevaPartida(const jugador&);
+
+    // Mueve al jugador en determinada dirección.
+    void mover(const direccion);
+
+    // Devuelve una referencia inmutable al mapa.
+    const Mapa mapa() const;
+
+    // Devuelve true si hay una partida en curso.
+    bool alguienJugando() const;
+
+    // Devuelve una referencia inmutable al jugador actual.
+    const jugador jugadorActual() const;
+
+    // Devuelve una referencia inmutable a la partida actual.
+    const Partida partidaActual() const;
+
+    // Devuelve una referencia inmutable al ranking.
+    const ranking ranking() const;
+
+    // Devuelve una tupla con el nombre y puntaje del jugador que
+    // supera inmediatamente al jugador actual, si es qe existe.
+    tuple<jugador, unsigned int> objetivo() const;
+
+private:
+    vector<vector<tuple<bool, bool, bool>>> _tablero;
+    Mapa* _mapa;
+    Partida* _partida;
+    bool _hayAlguien;
+    jugador _jugador;
+    // ranking _ranking;
 };
 
 
-#endif //TP4_PACALGO2_FICHIN_H
+#endif
