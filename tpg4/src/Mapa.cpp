7d55016597179622871bcfbd25ae50c9b9da3956
Guillermo Reyna <guille.j.reyna@gmail.com>
2021-06-12 21:12:05 -0300

    mapa

--- tpg4/src/Mapa.cpp
+++ tpg4/src/Mapa.cpp
@@ -1,1 +1,34 @@
 #include "Mapa.h"
+
+Mapa::Mapa(Nat largo, Nat alto, Coordenada inicio, Coordenada llegada,
+           set<Coordenada> paredes, set<Coordenada> fantasmas, set<Coordenada> chocolates):
+               _largo(largo), _alto(alto), _inicio(inicio), _llegada(llegada),
+               _paredes(paredes), _fantasmas(fantasmas), _chocolates(chocolates){}
+
+const Nat Mapa::largo() const {
+    return _largo;
+}
+
+const Nat Mapa::alto() const {
+    return _alto;
+}
+
+const Coordenada& Mapa::inicio() const{
+    return _inicio;
+}
+
+const Coordenada& Mapa::llegada() const{
+    return _llegada;
+}
+
+const set<Coordenada>& Mapa::chocolates() const{
+    return _chocolates;
+}
+
+const set<Coordenada>& Mapa::fantasmas() const{
+    return _fantasmas;
+}
+
+const set<Coordenada>& Mapa::paredes() const{
+    return _paredes;
+}
--- tpg4/src/Mapa.h
+++ tpg4/src/Mapa.h
@@ -1,9 +1,24 @@
 #ifndef TP4_PACALGO2_MAPA_H
 #define TP4_PACALGO2_MAPA_H
-
+#include "Tipos.h"
+#include <set>
 
 class Mapa {
+public:
+    Mapa(Nat largo, Nat alto, Coordenada inicio, Coordenada llegada, set<Coordenada> paredes, set<Coordenada> fantasmas,
+         set<Coordenada> chocolates);
+    const Nat largo() const;
+    const Nat alto() const;
+    const Coordenada& inicio() const;
+    const Coordenada& llegada() const;
+    const set<Coordenada>& chocolates() const;
+    const set<Coordenada>& paredes() const;
+    const set<Coordenada>& fantasmas() const;
 
+private:
+    Nat _largo, _alto;
+    Coordenada _inicio, _llegada;
+    set<Coordenada> _paredes, _fantasmas, _chocolates;
 };
 
 
